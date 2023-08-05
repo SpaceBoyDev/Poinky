8d8c7a02ee8d9be4633809308413e53a90780a47
Darío Martínez López <dario.mart.lop.03@gmail.com>
2023-08-05 03:28:13 +0200

    Un poquito de arte
    Prueba de como será el arte

--- PoinKy - Android/Assets/_Data/Scripts/UI/Buttons/ButtonAnimations.cs
+++ PoinKy - Android/Assets/_Data/Scripts/UI/Buttons/ButtonAnimations.cs
@@ -4,28 +4,38 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
 using UnityEngine.Serialization;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
+using Image = UnityEngine.UI.Image;
 
 public class ButtonAnimations : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler,
     IPointerUpHandler
 {
-   // private RectTransform rectTransform;
+    [SerializeField] private GameObject parentMenu;
+    [SerializeField] private bool isCanvasWorld;
+    [SerializeField] private bool hasShadow;
+
+    [SerializeField] private Vector3 shadowPosition;
+    [SerializeField] private Vector3 shadowScale;
+
+    [Header("Set alternative start values")]
     [SerializeField] private Vector3 setStartRotation;
 
-    
-    [Header("Start thingies")]
+    [Header("Start values")]
     private Vector3 startPosition;
     private Vector3 startScale;
     private Vector3 startRotation;
     
     [Header("Duration")]
     public float duration;
+    [SerializeField] private float positionDuration;
+    [SerializeField] private float scaleDuration;
     [SerializeField] private float rotationDuration;
-    
-    [SerializeField] private bool isCanvasWorld = true;
-    [SerializeField] private Vector3 newRotation;
+
+    [Header("End values (added to start values)")]
     [SerializeField] private Vector3 newPosition;
     [SerializeField] private Vector3 newScale; 
+    [SerializeField] private Vector3 newRotation;
     private void Start()
     {
         transform.eulerAngles = setStartRotation;
@@ -36,6 +46,40 @@ public class ButtonAnimations : MonoBehaviour, IPointerEnterHandler, IPointerExi
         
         transform.DORotate(startRotation + newRotation, rotationDuration, RotateMode.Fast).SetLoops(-1, LoopType.Yoyo)
             .SetRelative(false).SetEase(Ease.Linear);
+
+        if (hasShadow) //not used for now
+        {
+            GameObject buttonShadow = new GameObject(gameObject.name + " Shadow");
+            
+            buttonShadow.AddComponent<Image>();
+            buttonShadow.GetComponent<Image>().sprite = GetComponent<Image>().sprite;
+            buttonShadow.GetComponent<Image>().color = Color.black;
+            buttonShadow.GetComponent<RectTransform>().sizeDelta = gameObject.GetComponent<RectTransform>().sizeDelta;
+            buttonShadow.transform.position = transform.position - shadowPosition;
+            buttonShadow.transform.SetParent(parentMenu.transform);
+            buttonShadow.transform.localScale = transform.localScale;
+            
+            transform.SetParent(buttonShadow.transform);
+            
+            
+            buttonShadow.transform.eulerAngles = setStartRotation;
+            buttonShadow.transform.DORotate(startRotation + newRotation, rotationDuration, RotateMode.Fast).SetLoops(-1, LoopType.Yoyo)
+                .SetRelative(false).SetEase(Ease.Linear);
+        }
+    }
+    
+    
+    private void Update()
+    {
+        if (hasShadow)
+        {
+            UpdateShadow();
+        }
+    }
+
+    private void UpdateShadow()
+    {
+        
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -61,12 +105,12 @@ public class ButtonAnimations : MonoBehaviour, IPointerEnterHandler, IPointerExi
     {
         if (GetComponent<Button>().interactable == true)
         {
-            transform.DOScale(startScale + newScale, 0.2f).SetUpdate(true);
+            transform.DOScale(startScale + newScale, scaleDuration).SetUpdate(true);
         }
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        transform.DOScale(startScale, 0.2f).SetUpdate(true).SetEase(Ease.InQuad);
+        transform.DOScale(startScale, scaleDuration).SetUpdate(true).SetEase(Ease.InQuad);
     }
 }
